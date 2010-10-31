60c719b2c4ef4a214fb4bcdb9554cdb6dd7fb040
tommywalsh <ymmothslaw@gmail.com>
2010-10-31 19:49:44 -0400

    Live countdown to bus departure times.

--- src/com/github/tommywalsh/map/MBTAActivity.java
+++ src/com/github/tommywalsh/map/MBTAActivity.java
@@ -9,6 +9,7 @@ import android.app.ListActivity;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.os.Bundle;
+import android.os.Handler;
 
 import android.util.Log;
 
@@ -16,45 +17,77 @@ import java.util.SortedSet;
 
 public class MBTAActivity extends ListActivity
 {
+    private SortedSet<Departure> m_departures = null;
+    private Handler m_handler = new Handler();
+    private ArrayAdapter<String> m_aa = null;
+
+    private Runnable m_updateDisplay = new Runnable() {
+	    public void run() {
+		if (m_departures != null) {
+		    m_aa.clear();
+		    long now = java.lang.System.currentTimeMillis();
+
+		    for (Departure d : m_departures) {
+			int secondsLeft = (int) ((d.when - now) / 1000);
+			if (secondsLeft > 0) {
+			    int hours = secondsLeft / 3600;
+			    int minutes = (secondsLeft - hours*3600) / 60;
+			    int seconds = (secondsLeft - hours*3600 - minutes*60);
+			    
+			    String mess = d.route + " to " + d.direction + " stops at " + d.where + " in ";
+			    if (hours > 0) {
+				mess += (new Integer(hours)).toString() + ":";
+				if (minutes < 10) {
+				    mess += "0";
+				}
+			    } 
+			    if (hours > 0 || minutes > 0) {
+				mess += (new Integer(minutes)).toString();
+			    }
+			    mess += ":";
+			    if (seconds < 10) {
+				mess += "0";
+			    }
+			    mess += (new Integer(seconds)).toString();
+			    
+			    m_aa.add(mess);
+			}
+		    }
+		    setListAdapter(m_aa);
+		    m_handler.postDelayed(this, 500);
+		}
+	    }
+	};
+
+    
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+	m_aa = new ArrayAdapter<String>(this, R.layout.listitem);
+    }
 
+    public void onResume() {
+	super.onResume();
+	    
 	MBTAParser parser = new MBTAParser();
-	long now = java.lang.System.currentTimeMillis();
 
 	ArrayAdapter<String> aa = new ArrayAdapter<String>(this, R.layout.listitem);
 	try {
-	    SortedSet<Departure> ds = parser.parse(MBTADataService.getPredictionStream());
-	    for (Departure d : ds) {
-		int secondsLeft = (int) ((d.when - now) / 1000);
-		int hours = secondsLeft / 3600;
-		int minutes = (secondsLeft - hours*3600) / 60;
-		int seconds = (secondsLeft - hours*3600 - minutes*60);
-		
-		String mess = d.route + " to " + d.direction + " stops at " + d.where + " in ";
-		if (hours > 0) {
-		    mess += (new Integer(hours)).toString() + ":";
-		    if (minutes < 10) {
-			mess += "0";
-		    }
-		} 
-		if (hours > 0 || minutes > 0) {
-		    mess += (new Integer(minutes)).toString();
-		}
-		mess += ":";
-		if (seconds < 10) {
-		    mess += "0";
-		}
-		mess += (new Integer(seconds)).toString();
-
-		Log.d("mbta", mess);
-		aa.add(mess);
-	    }
-	    setListAdapter(aa);
+	    m_departures = parser.parse(MBTADataService.getPredictionStream());
 	} catch (java.io.IOException e) {
 	}
+
+	m_handler.removeCallbacks(m_updateDisplay);
+	m_handler.postDelayed(m_updateDisplay, 100);
     }
 
+
+		
+    public void onPause() {
+	super.onPause();
+	m_handler.removeCallbacks(m_updateDisplay);
+	m_handler = null;
+    }
 }
+
 
 
