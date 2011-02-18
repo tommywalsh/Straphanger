818f35e6398c54d015b01664d5cffd657beede95
tommywalsh <ymmothslaw@gmail.com>
2011-02-17 20:30:36 -0500

    Remove all of the SD card classes

--- src/com/github/tommywalsh/mbta/Prediction.java
+++ src/com/github/tommywalsh/mbta/Prediction.java
@@ -1,21 +1,23 @@
-// Copyright 2010 Tom Walsh
+// Copyright 2011 Tom Walsh
 //
 // This program is free software released under version 3
 // of the GPL.  See file gpl.txt for more information.
 
 package com.github.tommywalsh.mbta;
 
-public class Departure extends DeparturePoint implements Comparable {
-
+public class Prediction implements Comparable {
     public long when;
+    public String stopTitle;
+    public String routeTitle;
+    public String routeDirection;
+
 
     public int compareTo(Object o) {
-	Departure other = (Departure) o;
+	Prediction other = (Prediction) o;
 	int result = (int) (when - other.when);
 	if (result == 0) {
-	    result = route.compareTo(other.route);
+	    result = routeTitle.compareTo(other.routeTitle);
 	}
 	return result;
     }
-
 }
