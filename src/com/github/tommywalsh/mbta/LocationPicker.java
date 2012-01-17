bf9528d8da0f28da41a7475e46ba620da06275e2
tommywalsh <ymmothslaw@gmail.com>
2012-01-16 21:54:25 -0500

    Clean up LocationPicker a bit * Remove superfluous "cancel" button (users can just hit "back")   to free up room for a bigger map * Restructure the code a bit, grouping it by functionality.   Should be easier to read and maintain. * No longer starts up in Downtown Boston all the time

--- src/com/github/tommywalsh/mbta/LocationPicker.java
+++ src/com/github/tommywalsh/mbta/LocationPicker.java
@@ -1,9 +1,9 @@
+package com.github.tommywalsh.mbta;
+
 // Copyright 2011-12 Tom Walsh
 //
 // This program is free software released under version 3
 // of the GPL.  See file gpl.txt for more information.
-
-package com.github.tommywalsh.mbta;
 
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
@@ -28,17 +28,39 @@ import android.graphics.Canvas;
 import android.graphics.Point;
 import android.graphics.Paint;
 
-// This activity allows the user to pick a location on a map.
+// This class presents a map and allows the user to select a location
+//
+// This class takes no input from the caller.
+//
+// If the user does pick a location, we "return" a RESULT_OK code with
+// the latitude and longitude stored in the returned intent.
+//
+// The map is hard-coded to start showing Downtown Boston.  If we get
+// a location fix, we scroll the map to show the current location
+// (unless the user has already moved/zoomed the map)
+//
+// The user selects a location by double-tap.
 public class LocationPicker extends MapActivity
 {
 
-    // This is the only class that knows about GeoPoints.
-    // Here are some utility functions to convert to/from lat/long pairs
-    private static GeoPoint locationToGeoPoint(Location loc) {
-	return new GeoPoint((int)(loc.getLatitude() * 1E6),
-			    (int)(loc.getLongitude() * 1E6));
+    // There is only one user operation: selecting a location
+    // Here we just need to package up that location into the
+    // intent, and "return" it to the caller
+    private void onLocationSelected(double lat, double lng) {
+        Intent i = new Intent();
+        i.putExtra("com.github.tommywalsh.mbta.Lat", lat);
+        i.putExtra("com.github.tommywalsh.mbta.Lng", lng);
+        setResult(RESULT_OK, i);
+        finish();
     }
 
+
+
+
+
+    // This is the only class that knows about GeoPoints.
+    // Here are some utility functions to convert to lat/long pairs,
+    // which is what the rest of this app uses
     private static double getLatitude(GeoPoint g) {
         return ( (double)(g.getLatitudeE6()) / 1.0E6);
     }
@@ -50,12 +72,36 @@ public class LocationPicker extends MapActivity
 
 
 
-    // This overlay sits on top of the map, and waits for the user to double-tap the desired location
+
+
+
+    // We'll delegate to the standard Google MapActivity for most of the work,
+    // but we need a couple of custom overlays.  One will show the user's current
+    // location, and the other will process the double-tap
+    private void initOverlays(MapView mapview) {
+
+	mapview.getOverlays().add(new LocationSelectionOverlay(this));
+
+        m_locationIndicatorOverlay = new MyLocationOverlay(this, mapview);
+	mapview.getOverlays().add(m_locationIndicatorOverlay);
+
+
+        // This guy will scroll to our current location as soon as we get a fix.
+        m_locationIndicatorOverlay.runOnFirstFix(new Runnable() {
+                public void run() {
+                    m_controller.animateTo(m_locationIndicatorOverlay.getMyLocation());
+                }});
+    }
+
+    // This overlay shows user's current location on the map
+    private MyLocationOverlay m_locationIndicatorOverlay = null;
+
+    // This overlay class reacts to double-taps
     public class LocationSelectionOverlay extends Overlay {
 
         // On the first tap (and every subsequent tap), we use the MapView's "onTap" API
         // to learn the position.  We could figure this out ourselves in onDoubleTap, but 
-        // it would involve lots of calculation.  This is easier
+        // it would involve lots of calculation.  This is easier.
         private GeoPoint m_tapLoc = null;
         @Override public boolean onTap(GeoPoint where, MapView mv) {
             m_tapLoc = where;
@@ -63,11 +109,11 @@ public class LocationPicker extends MapActivity
         }
             
 
-        // At double-tap time, we rely on the fact that the "onTap" code above has already saved
-        // our position.  Here we just need to send this position back to our caller, and finish the activity
+        // At double-tap time, we rely on the fact that the "onTap" code above
+        // has already saved our position.  Here we just need to unpackage the
+        // data, and send it on to the onLocationSelected code
         public class DoubleTapListener extends GestureDetector.SimpleOnGestureListener {
             public boolean onDoubleTap(MotionEvent e) {
-                Intent i = new Intent();
 		GeoPoint tapLocation = m_tapLoc;
 		
 		// Unfortunately, if you double-tap on the position indicator, the 
@@ -78,10 +124,9 @@ public class LocationPicker extends MapActivity
 		    tapLocation = m_locationIndicatorOverlay.getMyLocation();
 		}
 
-                i.putExtra("com.github.tommywalsh.mbta.Lat", getLatitude(tapLocation));
-                i.putExtra("com.github.tommywalsh.mbta.Lng", getLongitude(tapLocation));
-                setResult(RESULT_OK, i);
-                finish();
+                onLocationSelected(getLatitude(tapLocation),
+                                   getLongitude(tapLocation));
+
                 return true;
             }
         }
@@ -99,8 +144,7 @@ public class LocationPicker extends MapActivity
     }
 
 
-    // This overlay shows user's current location on the map
-    private MyLocationOverlay m_locationIndicatorOverlay = null;
+
 
     private MapController m_controller;
 
@@ -109,36 +153,12 @@ public class LocationPicker extends MapActivity
         setContentView(R.layout.map);
 	
 	
-	MapView mapview = (MapView) findViewById(R.id.mapView);
+        MapView mapview = (MapView) findViewById(R.id.mapView);
 	m_controller = mapview.getController();
-
 	mapview.setBuiltInZoomControls(true);
-	mapview.getOverlays().add(new LocationSelectionOverlay(this));
-
-        m_locationIndicatorOverlay = new MyLocationOverlay(this, mapview);
-	mapview.getOverlays().add(m_locationIndicatorOverlay);
-
-
-	if(!(m_locationIndicatorOverlay.runOnFirstFix(new AutoScroller()))) {
-	    m_controller.setZoom(16);
-	    m_controller.setCenter(new GeoPoint(42355500,-71060500)); // Downtown Crossing
-	}
-
-
-        Button cancelButton = (Button)findViewById(R.id.cancel_map);
-        cancelButton.setOnClickListener(new OnClickListener() {
-                public void onClick(View v) {
-                    finish();
-                }
-            });
+        initOverlays(mapview);
     }
 
-
-    private class AutoScroller implements Runnable {
-	public void run() {
-	    m_controller.animateTo(m_locationIndicatorOverlay.getMyLocation());
-	}
-    }
 
     @Override protected void onResume() {
         super.onResume();
@@ -147,9 +167,12 @@ public class LocationPicker extends MapActivity
     
     @Override protected void onPause() {
         super.onPause();
+        // Save battery by not caring about location when we're not active.
         m_locationIndicatorOverlay.disableMyLocation();
-    }    
+    }
 
+
+    // MapActivity requires this override.  We never display routes.
     @Override protected boolean isRouteDisplayed() { 
         return false;
     }
